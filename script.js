9ec4b6566f06e7a299b249a938aa297ce0921d09
IkrashHaroon <ikrashharoon1236@gmail.com>
2025-12-26 13:05:38 +0500

    Assignment 20

---
+++ script.js
@@ -0,0 +1,53 @@
+// // Q1
+// function currentDateandTime() {
+//     var now = new Date();
+//      document.write("Current Date and Time: " + now)
+// }
+
+// currentDateandTime();
+
+// //Q2
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+// var userName = firstName  + " " + lastName
+
+// // console.log(userName);
+// function greet() {
+//     alert(`Hello ${userName} Welcome`)
+// }
+// greet();
+
+// //Q3
+
+// var num1 = +prompt("Enter First number");
+// var num2 = +prompt("Enter Last number");
+// var sum2num = num1 + num2 
+
+// function addtoNumbers() {
+//   alert(`Sum of ${num1} and ${num2} is ${sum2num}`)
+// }
+
+// addtoNumbers();
+
+
+//Q4 
+var firstNum = +prompt("Enter First number");
+var lastNum = +prompt("Enter Last number");
+var operator = prompt("Enter Operator (+,-,/,*)")
+
+function calculator(){
+    if (operator === "+"){
+        alert(firstNum + lastNum)
+    }
+    else if (operator === "-"){
+        alert(firstNum - lastNum)
+    }
+    else if (operator === "/"){
+        alert(firstNum / lastNum)
+    }
+    else {
+        alert(firstNum * lastNum)
+    }
+}
+calculator();
